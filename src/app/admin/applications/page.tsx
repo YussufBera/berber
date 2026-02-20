176b0a43ef53fc62c8c5dd7bdd8a734e6e42672d
Yusuf Bera <yusufberadeveloper@gmail.com>
2026-02-20 16:43:54 +0100

    Update page.tsx

--- src/app/admin/applications/page.tsx
+++ src/app/admin/applications/page.tsx
@@ -1,29 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import DashboardLayout from "@/components/admin/DashboardLayout";
 import ApplicationManager from "@/components/admin/ApplicationManager";
-import { useRouter } from "next/navigation";
 
 export default function AdminApplicationsPage() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const router = useRouter();
-
-    useEffect(() => {
-        const checkAuth = () => {
-            const authStatus = localStorage.getItem("adminAuth");
-            if (authStatus === "true") {
-                setIsAuthenticated(true);
-            } else {
-                router.push("/admin/login");
-            }
-        };
-
-        checkAuth();
-    }, [router]);
-
-    if (!isAuthenticated) return null;
-
     return (
         <DashboardLayout>
             <ApplicationManager />
